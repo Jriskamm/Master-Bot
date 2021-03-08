4eefa9947394f2eeb7856b4216eb388692e4faf6
Jris Development <69320092+Jriskamm@users.noreply.github.com>
2021-03-08 13:03:19 +0000

    Bunch
    Changed Invite Link

--- commands/guild/invite.js
+++ commands/guild/invite.js
@@ -17,7 +17,7 @@ module.exports = class InviteCommand extends Command {
 
   async run(message) {
     //provides the link with admin permissions
-    const inviteURL = `https://discord.com/oauth2/authorize?client_id=745140889755844680&scope=bot&permissions=3525704`;
+    const inviteURL = `https://https://discord.com/oauth2/authorize?client_id=745140889755844680&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Foauth2%2Fauthorize%3Fclient_id%3D745140889755844680%26scope%3Dbot%26permissions%3D3525704&response_type=code&scope=identify%20applications.commands%20bot%20email%20guildsth2/authorize?client_id=745140889755844680&scope=bot&permissions=3525704`;
 
     const guildCacheMap = this.client.guilds.cache;
     const guildCacheArray = Array.from(guildCacheMap, ([name, value]) => ({
@@ -35,7 +35,7 @@ module.exports = class InviteCommand extends Command {
       .setImage('https://images-ext-2.discordapp.net/external/onJIE1enNij8oh8MOcP9o7Ds3lN2jKU7pWZsPOxqUHA/https/media.discordapp.net/attachments/601060403941736460/702530987598413864/Tw.gif')
       .setURL(inviteURL)
       .setThumbnail(this.client.user.displayAvatarURL())
-      .setDescription(`You can [Click me](https://discord.com/oauth2/authorize?client_id=745140889755844680&scope=bot&permissions=3525704) so that you can add me into your server <a:785584094637195304:794108590285520936>`
+      .setDescription(`You can [Click me](https://https://discord.com/oauth2/authorize?client_id=745140889755844680&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Foauth2%2Fauthorize%3Fclient_id%3D745140889755844680%26scope%3Dbot%26permissions%3D3525704&response_type=code&scope=identify%20applications.commands%20bot%20email%20guildsth2/authorize?client_id=745140889755844680&scope=bot&permissions=3525704) so that you can add me into your server <a:785584094637195304:794108590285520936>`
       )
       .setFooter(
         'Operated by ' + this.client.owners[0].username + ' since',
